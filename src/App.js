a1d7ae0808d8eff18573e8ff913e02a929631fa7
mikeybkats <michaelbkats@gmail.com>
2017-06-28 20:16:35 -0700

    set up the first parts of board

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
+import cardCover from './cover.svg';
+import defaultCard from './default.svg';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <section className="title-section">
-            <h1>BLACK JACK</h1>
-          </section>
-        </div>
-        <p className="App-intro">
-        
-        </p>
+    return(
+      <div>
+        <Header />
+        <GameBoard value="0"/>
       </div>
-    );
+    )
   }
 }
+
+class Header extends Component {
+  render() {
+    return(
+      <section className="title-section">
+        <h1>BLACK JACK</h1>
+      </section>
+    )
+  }
+}
+
+class GameBoard extends Component {
+  render(){
+    return(
+      <section className="game-board">
+        <div className="game-board-contents">
+          <h2>Dealer</h2>
+					<p>Score {this.props.value}</p>
+					<CardsDealt />
+        </div>
+        <div className="game-board-contents">
+          <h2>Player</h2>
+					<p>Score {this.props.value}</p>
+					<CardsDealt value="0"/>
+        </div>
+      </section>
+    )
+  }
+}
+
+function CardsDealt(props) {
+  return(
+			<div className="hand">
+				<img alt="card-cover" className="card" src={cardCover} />
+				<img alt="play-card" className="card" src={defaultCard}/>
+		 </div> 	
+	)
+}
+
 
 export default App;
